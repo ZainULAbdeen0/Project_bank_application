982991d4ed606029079d48fc5944795e59b36d11
ZainULAbdeen0 <mujtabazain183@gmail.com>
2024-10-01 23:40:06 +0500

    implement some major ui changes.

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -59,7 +59,7 @@ const PlaidLink = ({user , variant}:PlaidLinkProps) => {
                         width={24}
                         height={24}
                     />
-                <p className='text-[16px] font-semibold text-black-2'>Connect bank</p>
+                <p className='text-[16px] font-semibold text-black-2 max-2xl:hidden'>Connect bank</p>
             </Button>
         )
           
